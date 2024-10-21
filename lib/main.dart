d0b37c6c7250171cea46eec0b29b0a8b551002d6
Connor Sullivan <connorssullivan25@gmail.com>
2024-10-21 16:27:02 -0400

    Real Time Database Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_database/firebase_database.dart'; // Import for Realtime Database
+
 
 import 'app.dart';
 
