52ff5924d50689ba2b6f16599cacc66eb006290d
Connor Sullivan <connorssullivan25@gmail.com>
2024-10-26 19:49:44 -0400

    Comit before merge

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,28 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown, // Optional: Allow upside-down portrait
   ]);
-  await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: 'AIzaSyCGh-3Xkhn-VY3nRxxLHfBypwOvlMZp5zA',
-        appId: '1:731242768427:android:f3ba289b78e4679fdca26e',
-        messagingSenderId: '383665209766',
-        projectId: 'prairiepatrol',
-        storageBucket: 'prairiepatrol.appspot.com',
-      )
-  );
+  try {
+  if (Firebase.apps.isEmpty) {
+    print('Firebase Initalizing');
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+          apiKey: 'AIzaSyCGh-3Xkhn-VY3nRxxLHfBypwOvlMZp5zA',
+          appId: '1:731242768427:android:f3ba289b78e4679fdca26e',
+          messagingSenderId: '383665209766',
+          projectId: 'prairiepatrol',
+          storageBucket: 'prairiepatrol.appspot.com',
+        )
+    );
+  }else{
+    print('Firebase already Initalized');
+  }
+  } catch (e) {
+    if (e.toString().contains('[core/duplicate-app]')) {
+      print('Firebase app already exists');
+    } else {
+      print('Error initializing Firebase: $e');
+    }
+  }
   runApp(const MyApp());
 }
 
