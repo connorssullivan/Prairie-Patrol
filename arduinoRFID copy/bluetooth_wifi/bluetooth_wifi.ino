c0ec4e6c06cdb74138be6f46d5d0ca132d217efb
Jude <judemaggitti123@gmail.com>
2025-02-07 00:35:56 -0500

    combined the bluetooth and magnet funtionality

--- arduinoRFID copy/bluetooth_wifi/bluetooth_wifi.ino
+++ arduinoRFID copy/bluetooth_wifi/bluetooth_wifi.ino
@@ -3,11 +3,22 @@
 #include <WiFi.h>
 #include <ArduinoBLE.h>
 #include <Firebase.h>
+#include <MFRC522.h>
 
+#define MAGNET_LOCK 5
+#define SS_PIN 10
+#define RST_PIN 9
 
-BLEService connectionService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service 
+MFRC522 rfid(SS_PIN, RST_PIN);
+MFRC522::MIFARE_Key key;
+Firebase fb(REFERENCE_URL);
+
+#define MAGNET_PIN 5
+
+BLEService connectionService("19B10000-E8F2-537E-4F6C-D104768A1214");
 BLEStringCharacteristic  connectionCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 30);
 
+const String JUDES_FOLDER = "JudesFolder";
 
 void setup() {
   Serial.begin(9600);
@@ -22,10 +33,13 @@ void setup() {
   connectionService.addCharacteristic(connectionCharacteristic);
   BLE.addService(connectionService);
   BLE.advertise();
+
+  RFIDSetup();
 }
 
 void loop() {
   bluetoothConnect();
+  trapMode();
   delay(500);
 }
 
@@ -67,4 +81,93 @@ void bluetoothConnect() {
   } else {
     Serial.println("Failed to connect to device!2");
   }
+}
+
+void trapMode() {
+  bool tA = fb.getBool(getPath(JUDES_FOLDER, "selectedDog")+"/trapActive");
+  bool tO = fb.getBool(getPath(JUDES_FOLDER, "selectedDog")+"/trapOpen");
+
+  if(tA&&tO) {
+    if(digitalRead(MAGNET_LOCK) == LOW){
+      digitalWrite(MAGNET_LOCK, HIGH);
+    }
+    Serial.println("Active Trap");
+    void RFIDSetup();
+  } else  {
+    digitalWrite(MAGNET_LOCK, LOW);
+  }
+}
+
+void RFIDSetup() {
+  if(rfid.PICC_IsNewCardPresent()) {
+    Serial.println("test");
+    if(rfid.PICC_ReadCardSerial())  {
+      String scannedRFID = "";
+      for(byte i = 0; i < rfid.uid.size; i++) {
+        scannedRFID += String(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println("Scanned RFID Tag: "+scannedRFID);
+      if(selectedDogsRFID(scannedRFID)) {
+        updateDB();
+      } else {      
+        Serial.print(F("Unknown RFID Tag UID: "));
+        printHex(rfid.uid.uidByte, rfid.uid.size);
+        Serial.println();
+      }      
+      rfid.PICC_HaltA();
+      rfid.PCD_StopCrypto1();
+    }
+  }
+}
+
+void updateDB() {
+  bool automatic = fb.getBool(getPath(JUDES_FOLDER, "selectedDog")+"/auto");
+  if(automatic) {
+    digitalWrite(MAGNET_LOCK, LOW);
+    Serial.println("Magnet OFF");
+    fb.setBool(getPath(JUDES_FOLDER, "selectedDog") + "/trapOpen", false);
+  } else{
+    Serial.println("this would send notification to phone");
+  }
+}
+
+String getPath(const String& folder, const String& subfolder) {
+  return folder + "/" + subfolder;
+}
+
+bool selectedDogsRFID(String scannedTag) {
+  String listRfid = fb.getString(getPath(JUDES_FOLDER, "selectedDog") + "/listRfid");
+
+  if (listRfid.length() > 0) {
+    Serial.println("Retrieved RFID list from Firebase: " + listRfid);
+
+    listRfid.replace("[", "");
+    listRfid.replace("]", "");
+    listRfid.replace("\"", "");
+
+    int startIndex = 0;
+    while (startIndex >= 0) {
+      int endIndex = listRfid.indexOf(',', startIndex);
+      String rfidTag = (endIndex == -1) ? listRfid.substring(startIndex) : listRfid.substring(startIndex, endIndex);
+
+      rfidTag.trim();
+      if (rfidTag.equalsIgnoreCase(scannedTag)) {
+        Serial.println("RFID match found!");
+        return true;
+      }
+      startIndex = (endIndex == -1) ? -1 : endIndex + 1;
+    }
+    Serial.println("No matching RFID found.");
+    return false;
+  } else {
+    Serial.println("Failed to get RFID list from Firebase or list is empty.");
+    return false;
+  }
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
 }
