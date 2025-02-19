a2c7f1f277da6238ec5d000e9117fe157d0968c7
Connor Sullivan <connorssullivan25@gmail.com>
2025-02-19 14:29:06 -0500

    notifications

--- lib/screens/home/views/notif_screen.dart
+++ lib/screens/home/views/notif_screen.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
 
-class NotificationScreen extends StatelessWidget {
+import '../../../services/dogs_service.dart';
+import '../../../services/rt_dogs_service.dart';
+
+class NotificationScreen extends StatefulWidget {
   const NotificationScreen({Key? key}) : super(key: key);
+
+  @override
+  State<NotificationScreen> createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  //Gets notifications
+  final dogService = RTDogsService();
+  List<Map<String, dynamic>> notifications = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchNotifications();
+  }
+
+  Widget customBox(String title, String message, String timestamp) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.blueAccent),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(title),
+    );
+  }
+
+  // Fetch notifications from Firebase
+  Future<void> _fetchNotifications() async {
+    //Pause for 5 seconds
+    await Future.delayed(Duration(seconds: 5));
+    var notificationData = await dogService.getNotifications();
+    if (notificationData != null && notificationData is Map<dynamic, dynamic>) {
+      setState(() {
+        notifications = notificationData.entries.map((entry) {
+          Map<String, dynamic> value = Map<String, dynamic>.from(entry.value);
+          return {
+            'id': entry.key,
+            'title': value['title'] ?? 'No title',
+            'message': value['message'] ?? 'No message',
+            'timestamp': value['timestamp'] ?? 'No timestamp',
+          };
+        }).toList();
+      });
+      print(notifications);
+    } else {
+      print('No notifications found.');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +63,7 @@ class NotificationScreen extends StatelessWidget {
         title: const Text('Notifications'),
       ),
       body: Center(
-        child: const Text('Notification Screen'),
+        child: Text(notifications.toString()),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
